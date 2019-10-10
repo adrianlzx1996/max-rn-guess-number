5cd5bdc61f747ed1f9e60305d3eb2c4aaf4633e0
Adrian Leong <leongadrian36@gmail.com>
2019-10-10 09:06:04 +0800

    added game over logic and some styling

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
+import Colors from "../constants/colors";
+
+const GameOverScreen = props => {
+	return (
+		<View style={styles.screen}>
+			<TitleText>The Game is Over!</TitleText>
+			<View style={styles.imageContainer}>
+				<Image
+					// source={require("../assets/success.png")}
+					source={{
+						uri:
+							"https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg"
+					}}
+					style={styles.image}
+				/>
+			</View>
+			<View style={styles.resultContainer}>
+				<BodyText style={styles.resultText}>
+					Your phone needed{" "}
+					<Text style={styles.highlight}>{props.roundsNumber}</Text>{" "}
+					rounds to guess the number{" "}
+					<Text style={styles.highlight}>{props.userNumber}</Text>.
+				</BodyText>
+				<Button
+					title="NEW GAME"
+					onPress={() => {
+						props.onRestart();
+					}}
+				/>
+			</View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center"
+	},
+	image: {
+		width: "100%",
+		height: "100%"
+	},
+	imageContainer: {
+		width: 300,
+		height: 300,
+		borderRadius: 150,
+		borderWidth: 3,
+		borderColor: "black",
+		overflow: "hidden",
+		marginVertical: 20
+	},
+	resultContainer: {
+		marginHorizontal: 30,
+		marginVertical: 15
+	},
+	resultText: {
+		textAlign: "center"
+	},
+	highlight: {
+		color: Colors.primary,
+		fontFamily: "open-sans-bold"
+	}
+});
+
+export default GameOverScreen;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,20 +1,108 @@
-import React from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import {
+	Alert,
+	Button,
+	Keyboard,
+	StyleSheet,
+	Text,
+	TouchableWithoutFeedback,
+	View
+} from "react-native";
+
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
 import Card from "../components/Card";
+import Input from "../components/Input";
+import Colors from "../constants/colors";
+import NumberContainer from "../components/NumberContainer";
 
 const StartGameScreen = props => {
-	return (
-		<View style={styles.screen}>
-			<Text style={styles.title}>Start a New Game!</Text>
-			<Card style={styles.inputContainer}>
-				<Text>Select A Number</Text>
-				<TextInput />
-				<View style={styles.buttonContainer}>
-					<Button title="Reset" onpress={() => {}} />
-					<Button title="Confirm" onpress={() => {}} />
-				</View>
+	const [enteredValue, setEnteredValue] = useState("");
+	const [confirmed, setConfirmed] = useState(false);
+	const [selectedNumber, setSelectedNumber] = useState("");
+
+	const numberInputHandler = text => {
+		setEnteredValue(text.replace(/[^0-9]/g, ""));
+	};
+
+	const resetInputHandler = () => {
+		setEnteredValue("");
+		setConfirmed(false);
+	};
+
+	const confirmInputHandler = () => {
+		const chosenNumber = parseInt(enteredValue);
+		if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+			Alert.alert(
+				"Invalid Number!",
+				"Number has to be a number between 1 and 99",
+				[
+					{
+						text: "Okay",
+						style: "destructive",
+						onPress: resetInputHandler
+					}
+				]
+			);
+			return;
+		}
+		setConfirmed(true);
+		setSelectedNumber(chosenNumber);
+		setEnteredValue("");
+		Keyboard.dismiss();
+	};
+
+	let confirmedOutput;
+
+	if (confirmed) {
+		confirmedOutput = (
+			<Card style={styles.summaryContainer}>
+				<BodyText>You selected</BodyText>
+				<NumberContainer>{selectedNumber}</NumberContainer>
+				<Button
+					title="Start Game"
+					onPress={() => props.onStartGame(selectedNumber)}
+				/>
 			</Card>
-		</View>
+		);
+	}
+
+	return (
+		<TouchableWithoutFeedback
+			onPress={() => {
+				Keyboard.dismiss();
+			}}
+		>
+			<View style={styles.screen}>
+				<TitleText style={styles.title}>Start a New Game!</TitleText>
+				<Card style={styles.inputContainer}>
+					<BodyText>Select A Number</BodyText>
+					<Input
+						style={styles.input}
+						blurOnSubmit
+						autoCapitalize="none"
+						autoCorrect={false}
+						keyboardType="number-pad"
+						maxLength={2}
+						onChangeText={numberInputHandler}
+						value={enteredValue}
+					/>
+					<View style={styles.buttonContainer}>
+						<Button
+							title="Reset"
+							onPress={resetInputHandler}
+							color={Colors.accent}
+						/>
+						<Button
+							title="Confirm"
+							onPress={confirmInputHandler}
+							color={Colors.primary}
+						/>
+					</View>
+				</Card>
+				{confirmedOutput}
+			</View>
+		</TouchableWithoutFeedback>
 	);
 };
 
@@ -31,14 +119,26 @@ const styles = StyleSheet.create({
 		justifyContent: "space-between",
 		paddingHorizontal: 15
 	},
+	button: {
+		width: 100
+	},
 	inputContainer: {
 		width: 300,
 		maxWidth: "80%",
 		alignItems: "center"
 	},
+	input: {
+		width: 50,
+		textAlign: "center"
+	},
 	title: {
 		fontSize: 20,
-		marginVertical: 10
+		marginVertical: 10,
+		fontFamily: "open-sans-bold"
+	},
+	summaryContainer: {
+		marginTop: 20,
+		alignItems: "center"
 	}
 });
 
